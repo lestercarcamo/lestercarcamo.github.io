07810b2617516e698273195f48e25c646e6ae624
lestercarcamo <35443491+lestercarcamo@users.noreply.github.com>
2024-11-17 21:49:00 -0800

    combarela

---
+++ ch2_ cans_volume.py
@@ -0,0 +1,11 @@
+CAN_VOLUME = 0.355
+BOTTLE_VOLUME = 2.0
+
+# number of cans per pack
+cansPerPack = 6
+
+# calculate the total volume in the cans
+totalVolume = cansPerPack * CAN_VOLUME
+print("A six-pack of 12 ounce cans contains ", totalVolume, " liters.")
+
+
